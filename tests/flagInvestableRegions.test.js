d4ead35a0ddadab58a93e17aca7696d756e0c574
Felipe Araujo <felipe.peixoto.araujo@gmail.com>
2018-03-01 00:44:11 +0000

    fixing indentation

--- tests/flagInvestableRegions.test.js
+++ tests/flagInvestableRegions.test.js
@@ -2,100 +2,100 @@ var immo = require('../src/immo');
 
 test("empty list of investable regions doesn't change index", () => {
   expect(
-  	immo.flagInvestableRegions(
-	  	[], 
-	  	{
-	  		'london': {subregions: [], isInvestable: true},
-	  		'manchester': {subregions: [], isInvestable: false}
-	  	}
-  	)
+    immo.flagInvestableRegions(
+      [], 
+      {
+        'london': {subregions: [], isInvestable: true},
+        'manchester': {subregions: [], isInvestable: false}
+      }
+    )
   )
   .toEqual({
-		'london': {subregions: [], isInvestable: true},
-		'manchester': {subregions: [], isInvestable: false}
-	});
+    'london': {subregions: [], isInvestable: true},
+    'manchester': {subregions: [], isInvestable: false}
+  });
 });
 
 test("investable region and its subregions are marked as investable", () => {
   expect(
-  	immo.flagInvestableRegions(
-	  	['london'],
-	  	{
-	  		'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
-	  		'cardiff': {subregions: [], isInvestable: false},
-	  		'london': {subregions: ['camden'], isInvestable: false},
-	  		'camden': {subregions: ['camden town'], isInvestable: false},
-	  		'camden town': {subregions: [], isInvestable: false}
-	  	}
-  	)
+    immo.flagInvestableRegions(
+      ['london'],
+      {
+        'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
+        'cardiff': {subregions: [], isInvestable: false},
+        'london': {subregions: ['camden'], isInvestable: false},
+        'camden': {subregions: ['camden town'], isInvestable: false},
+        'camden town': {subregions: [], isInvestable: false}
+      }
+    )
   )
   .toEqual({
-		'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
-		'cardiff': {subregions: [], isInvestable: false},
-		'london': {subregions: ['camden'], isInvestable: true},
-		'camden': {subregions: ['camden town'], isInvestable: true},
-		'camden town': {subregions: [], isInvestable: true}
-	});
+    'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
+    'cardiff': {subregions: [], isInvestable: false},
+    'london': {subregions: ['camden'], isInvestable: true},
+    'camden': {subregions: ['camden town'], isInvestable: true},
+    'camden town': {subregions: [], isInvestable: true}
+  });
 });
 
 test("non-existant regions are ignored", () => {
   expect(
-  	immo.flagInvestableRegions(
-	  	['cardiff', 'bristol'], 
-	  	{
-	  		'london': {subregions: [], isInvestable: true},
-	  		'manchester': {subregions: [], isInvestable: false}
-	  	}
-  	)
+    immo.flagInvestableRegions(
+      ['cardiff', 'bristol'], 
+      {
+        'london': {subregions: [], isInvestable: true},
+        'manchester': {subregions: [], isInvestable: false}
+      }
+    )
   )
   .toEqual({
- 		'london': {subregions: [], isInvestable: true},
- 		'manchester': {subregions: [], isInvestable: false}
-	});
+    'london': {subregions: [], isInvestable: true},
+    'manchester': {subregions: [], isInvestable: false}
+  });
 });
 
 test("cyclical dependency won't get in an infinite loop and the regions will be flagged properly", () => {
   expect(
-  	immo.flagInvestableRegions(
-	  	['london'],
-	  	{
-	  		'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
-	  		'cardiff': {subregions: [], isInvestable: false},
-	  		'london': {subregions: ['camden'], isInvestable: false},
-	  		'camden': {subregions: ['camden town'], isInvestable: false},
-	  		'camden town': {subregions: ['london'], isInvestable: false}
-	  	}
-  	)
+    immo.flagInvestableRegions(
+      ['london'],
+      {
+        'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
+        'cardiff': {subregions: [], isInvestable: false},
+        'london': {subregions: ['camden'], isInvestable: false},
+        'camden': {subregions: ['camden town'], isInvestable: false},
+        'camden town': {subregions: ['london'], isInvestable: false}
+      }
+    )
   )
   .toEqual({
-		'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
-		'cardiff': {subregions: [], isInvestable: false},
-		'london': {subregions: ['camden'], isInvestable: true},
-		'camden': {subregions: ['camden town'], isInvestable: true},
-		'camden town': {subregions: ['london'], isInvestable: true}
-	});
+    'uk': {subregions: ['london', 'cardiff'], isInvestable: false},
+    'cardiff': {subregions: [], isInvestable: false},
+    'london': {subregions: ['camden'], isInvestable: true},
+    'camden': {subregions: ['camden town'], isInvestable: true},
+    'camden town': {subregions: ['london'], isInvestable: true}
+  });
 });
 
 test("separate trees will be flagged properly", () => {
   expect(
-  	immo.flagInvestableRegions(
-	  	['london', 'cardiff'],
-	  	{
-	  		'cardiff': {subregions: ['lisvane'], isInvestable: false},
-	  		'lisvane': {subregions: [], isInvestable: false},
-	  		'london': {subregions: ['camden', 'islington'], isInvestable: false},
-	  		'islington': {subregions: [], isInvestable: false},
-	  		'camden': {subregions: ['camden town'], isInvestable: false},
-	  		'camden town': {subregions: ['london'], isInvestable: false}
-	  	}
-  	)
+    immo.flagInvestableRegions(
+      ['london', 'cardiff'],
+      {
+        'cardiff': {subregions: ['lisvane'], isInvestable: false},
+        'lisvane': {subregions: [], isInvestable: false},
+        'london': {subregions: ['camden', 'islington'], isInvestable: false},
+        'islington': {subregions: [], isInvestable: false},
+        'camden': {subregions: ['camden town'], isInvestable: false},
+        'camden town': {subregions: ['london'], isInvestable: false}
+      }
+    )
   )
   .toEqual({
-	  		'cardiff': {subregions: ['lisvane'], isInvestable: true},
-	  		'lisvane': {subregions: [], isInvestable: true},
-	  		'london': {subregions: ['camden', 'islington'], isInvestable: true},
-	  		'islington': {subregions: [], isInvestable: true},
-	  		'camden': {subregions: ['camden town'], isInvestable: true},
-	  		'camden town': {subregions: ['london'], isInvestable: true}
-	});
+        'cardiff': {subregions: ['lisvane'], isInvestable: true},
+        'lisvane': {subregions: [], isInvestable: true},
+        'london': {subregions: ['camden', 'islington'], isInvestable: true},
+        'islington': {subregions: [], isInvestable: true},
+        'camden': {subregions: ['camden town'], isInvestable: true},
+        'camden town': {subregions: ['london'], isInvestable: true}
+  });
 });
--- tests/getIndexedRegions.test.js
+++ tests/getIndexedRegions.test.js
@@ -7,32 +7,32 @@ test('empty list of regions returns empty index', () => {
 
 test('regions get properly indexed', () => {
   expect(immo.getIndexedRegions([
-  		{name: 'uk', parent: ''},
-  		{name: 'cardiff', parent: 'uk'},
-  		{name: 'london', parent: 'uk'},
-  		{name: 'camden', parent: 'london'},
-  		{name: 'camden town', parent: 'camden'}
-  	])
+      {name: 'uk', parent: ''},
+      {name: 'cardiff', parent: 'uk'},
+      {name: 'london', parent: 'uk'},
+      {name: 'camden', parent: 'london'},
+      {name: 'camden town', parent: 'camden'}
+    ])
   )
   .toEqual({
-  	'uk': {subregions: ['cardiff', 'london'], isInvestable: false},
-		'cardiff': {subregions: [], isInvestable: false},
-	  'london': {subregions: ['camden'], isInvestable: false},
-	  'camden': {subregions: ['camden town'], isInvestable: false},
-	  'camden town': {subregions: [], isInvestable: false}
-	});
+    'uk': {subregions: ['cardiff', 'london'], isInvestable: false},
+    'cardiff': {subregions: [], isInvestable: false},
+    'london': {subregions: ['camden'], isInvestable: false},
+    'camden': {subregions: ['camden town'], isInvestable: false},
+    'camden town': {subregions: [], isInvestable: false}
+  });
 });
 
 test("top level region doesn't get indexed", () => {
   expect(immo.getIndexedRegions([
-  		{name: 'cardiff', parent: ''},
-  		{name: 'london', parent: ''},
-  		{name: 'manchester', parent: ''}
-  	])
+      {name: 'cardiff', parent: ''},
+      {name: 'london', parent: ''},
+      {name: 'manchester', parent: ''}
+    ])
   )
   .toEqual({
-		'cardiff': {subregions: [], isInvestable: false},
-	  'london': {subregions: [], isInvestable: false},
-	  'manchester': {subregions: [], isInvestable: false}
-	});
+    'cardiff': {subregions: [], isInvestable: false},
+    'london': {subregions: [], isInvestable: false},
+    'manchester': {subregions: [], isInvestable: false}
+  });
 });
